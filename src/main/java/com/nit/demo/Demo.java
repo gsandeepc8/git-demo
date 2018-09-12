1d970c1bf1baa6764744c01bd5857fb65c93161c
sandeep <gsandeepc8@gmail.com>
2018-09-12 16:44:09 +0530

    added git-demo project to localrepository

---
+++ src/main/java/com/nit/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.nit.demo;
+
+public class Demo {
+public static void dummyMsg(String data) {
+	System.out.println("Dummy Data: "+data);
+}
+	public static void main(String[] args) {
+        dummyMsg("Good Evening...");
+
+	}
+
+}
