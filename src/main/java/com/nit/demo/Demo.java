6a8e656822c551f441d44eb9fd76a1c5ed1b2174
sandeep <gsandeepc8@gmail.com>
2018-09-12 17:08:10 +0530

    added if condition to verify the given data

--- src/main/java/com/nit/demo/Demo.java
+++ src/main/java/com/nit/demo/Demo.java
@@ -2,7 +2,9 @@ package com.nit.demo;
 
 public class Demo {
 public static void dummyMsg(String data) {
+	if(data.equalsIgnoreCase("Good Evening...")) {
 	System.out.println("Dummy Data: "+data);
+	}
 }
 	public static void main(String[] args) {
         dummyMsg("Good Evening...");
