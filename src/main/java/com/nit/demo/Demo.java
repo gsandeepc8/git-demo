e8071540a519795c90d00f3128532552419262fe
sandeep <gsandeepc8@gmail.com>
2018-09-13 11:14:39 +0530

    Iam removing the system.out.println() in one method

--- src/main/java/com/nit/demo/Demo.java
+++ src/main/java/com/nit/demo/Demo.java
@@ -1,14 +1,16 @@
 package com.nit.demo;
 
 public class Demo {
-public static void dummyMsg(String data) {
-	if(data.equalsIgnoreCase("Good Evening...")) {
-		System.out.println("Iam adding another statement in this method");
-	System.out.println("Dummy Data: "+data);
+	public static void dummyMsg(String data) {
+		if (data.equalsIgnoreCase("Good Evening...")) {
+			
+			
+			System.out.println("Dummy Data: " + data);
+		}
 	}
-}
+
 	public static void main(String[] args) {
-        dummyMsg("Good Evening...");
+		dummyMsg("Good Evening...");
 
 	}
 
